a336471bd0cda0d45f14d6283042c2be7b798c01
Elisa Kauffmann <elisa.kauffmann.f@gmail.com>
2017-04-05 18:37:19 +0000

    add heroku host to settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's2ri83u7lv*rc+g&mln&_@e91btu42gk%+xbpwedk(@hcm6aq$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [u'to-doge-listalian.herokuapp.com',]
 
 
 # Application definition
